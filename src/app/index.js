87b1f582020cbc658f14323b0595234761cf6d6d
dudaroch <134696629+dudaroch@users.noreply.github.com>
2024-08-20 12:19:58 +0000

    [feat] expo-router

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha primeira janela</Text>
+      <Text>Minha primeira janela - Alterada</Text>
       <StatusBar style="auto" />
     </View>
   );
